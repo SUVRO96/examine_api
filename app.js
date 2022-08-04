52186e555c3b651e6777ef42ed793ff7fd653244
Suvro96 <uniquesuvro@gmail.com>
2022-08-04 15:32:13 +0530

    new project

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log("hello");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// require("dotenv").config({path:".env"});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  try {
+    console.log("examine API is running");
+    res.send("<h1>examine API is running</h1>");
+  } catch {}
+});
+
+const userRoute = require("./routes/users");
+app.use("/users", userRoute);
+
+// app.listen(process.env.APP_PORT);
+app.listen(4000);
